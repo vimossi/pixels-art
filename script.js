b468725343eba7ba1c7c9602febd3de864dd83c8
ViniciusMossi <vini_agg@hotmail.com>
2021-05-05 19:17:58 -0400

    teste requisito 2

---
+++ script.js
@@ -0,0 +1,17 @@
+let paletas = document.getElementById("color-palete").style.display = "inline-block";
+let color = document.getElementsByClassName(".color")
+color.style.border = "1px solid black";
+color.style.width = "40px";
+color.style.height = "40px";
+
+
+
+
+// insere cores
+const createColors = document.querySelectorAll('.color');
+const selectedColor = ['black', 'yellow', 'brown', 'green'];
+
+for (let index in selectedColor) {
+  createColors[index].style.backgroundColor = selectedColor[index];
+}
+createColors[0].classList.add("selected");
