cbb4add21acf91ba3d8e294c577c5672fca0b6e8
ViniciusMossi <vini_agg@hotmail.com>
2021-05-07 15:38:40 -0400

    Verifica requisito 2 e 3

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
-const paletas = document.getElementById('color-palete').style.display = 'inline-block';
+const paletas = document.getElementById('color-palette');
+paletas.style.display = 'inline-block';
 const color = document.getElementsByClassName('.color')
-color.style.border = '1px solid black';
-color.style.width = '40px';
-color.style.height = '40px';
 
 
 
-
-// insere cores
-const createColors = document.querySelectorAll('.color');
-const selectedColor = ['black', 'yellow', 'brown', 'green'];
-
-for (let index in selectedColor) {
-  createColors[index].style.backgroundColor = selectedColor[index];
-}
-createColors[0].classList.add('selected');
