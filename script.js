b0cd4ecd8aec5b0c769908d1d7717e33936d876f
ViniciusMossi <vini_agg@hotmail.com>
2021-05-07 16:11:24 -0400

    verifica requisito 4 e 5

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-const paletas = document.getElementById('color-palette');
-paletas.style.display = 'inline-block';
-const color = document.getElementsByClassName('.color')
+let quadro = document.getElementById("pixel-board");
 
-
-
+function square(num) {
+  for(let index = 0; index < num; index += 1) {
+    let linha = document.createElement('div');
+    linha.className = 'pixel';
+    quadro.appendChild(linha);
+    
+  }
+}
+square(5);
