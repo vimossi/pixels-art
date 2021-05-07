92cc62f1d7fe0bf2d5e99b54b03afd9b77441d1a
ViniciusMossi <vini_agg@hotmail.com>
2021-05-07 17:12:34 -0400

    verifica requisito 4 e 5

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-let quadro = document.getElementById("pixel-board");
-
+const quadro = document.getElementById('pixel-board');
 function square(num) {
-  for(let index = 0; index < num; index += 1) {
-    let linha = document.createElement('div');
-    linha.className = 'pixel';
+  for (let index = 0; index < num; index += 1) {
+    const linha = document.createElement('div');
     quadro.appendChild(linha);
-    
+  for (let index = 0; index < num; index +=1) {
+  const coluna = document.createElement('div');
+  coluna.className = 'pixel';
+  quadro.appendChild(coluna);
   }
 }
+}
 square(5);
+
+
