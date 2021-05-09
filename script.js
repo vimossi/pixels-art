e6e0cf807a07def2bdf19ef6d97c55137d387040
ViniciusMossi <vini_agg@hotmail.com>
2021-05-09 17:33:42 -0400

    corrige identaçao

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function square(num) {
   for (let index = 0; index < num; index += 1) {
     const linha = document.createElement('div');
     quadro.appendChild(linha);
-      for (let index = 0; index < num; index +=1) {
+    for (let index2 = 0; index2 < num; index2 += 1) {
       const coluna = document.createElement('div');
       coluna.className = 'pixel';
       quadro.appendChild(coluna);
@@ -11,7 +11,6 @@ function square(num) {
   }
 }
 square(5);
-
 
 // a partir daqui eu só consegui fazer observando o repositorio do Felipe Carvalho : https://github.com/tryber/sd-011-project-pixels-art/tree/felipe-carvalho-project-pixels-art
 
@@ -25,7 +24,7 @@ for (let index = 0; index < selectableColors.length; index += 1) {
   selectableColors[index].addEventListener('click', makeSelected);
 }
 // ele armazenou a seleção do .color e criou uma função evento, fazendo que todos com classe .selected index fossem atribuidas a 'color'. Depois criou um laço de repetição para que o evento funcionasse com todos da mesma classe. 
-function colorindo(event) {
+function colorindo(event){
   const pixel = event.target;
   pixel.style.backgroundColor = document.querySelectorAll('.selected')[0].id;
 }
@@ -33,4 +32,4 @@ const pixels = document.querySelectorAll('.pixel');
 for (let index = 0; index < pixels.length; index += 1) {
   pixels[index].addEventListener('click', colorindo);
 }
-// estudo // vou refatorar com outro codigo 
+// estudo // vou refatorar com outro codigo
